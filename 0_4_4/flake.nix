a1ef915d4905002c62c22451df5ad99ca90d9c76
hugosenari <hugosenari@gmail.com>
2022-05-03 01:31:18 -0300

    chore: re index flippy

--- 0_4_4/flake.nix
+++ 0_4_4/flake.nix
@@ -45,13 +45,13 @@
   inputs."nimpng".inputs.nixpkgs.follows = "nixpkgs";
   inputs."nimpng".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
   
-  inputs."nim-snappy".owner = "nim-nix-pkgs";
-  inputs."nim-snappy".ref   = "master";
-  inputs."nim-snappy".repo  = "nim-snappy";
-  inputs."nim-snappy".dir   = "master";
-  inputs."nim-snappy".type  = "github";
-  inputs."nim-snappy".inputs.nixpkgs.follows = "nixpkgs";
-  inputs."nim-snappy".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  inputs."snappy".owner = "nim-nix-pkgs";
+  inputs."snappy".ref   = "master";
+  inputs."snappy".repo  = "snappy";
+  inputs."snappy".dir   = "master";
+  inputs."snappy".type  = "github";
+  inputs."snappy".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."snappy".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
   
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
   let 
